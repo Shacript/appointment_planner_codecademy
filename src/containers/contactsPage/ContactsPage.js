6d4999b0081e3cf053c0832272a00879b3341823
Aphisit Likitwattanapaisarn <aphisit.heung@gmail.com>
2022-03-06 11:04:08 +0700

    remove unused import

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm";
-import { tileList } from "../../components/tileList";
 import { TileList } from "../../components/tileList/TileList";
 
 export const ContactsPage = ({ contacts, addContact }) => {
